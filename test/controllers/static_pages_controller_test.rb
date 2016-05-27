450d65a3adf5b045511c04a2613f6b6ff96695a6
cesswairimu <cessmbuguar@gmail.com>
2016-05-27 13:01:58 +0300

    created a full title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,30 +1,29 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  def setup
-  	@base_title = "Ruby on Rails Tutorial Sample App"
-  end
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on rails Tutorial Sample App" 
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | Ruby on rails Tutorial Sample App"
   end
   test "should get about" do
   	get :about
   	assert_response :success
-  	assert_select "title", "About | #{@base_title}"
+  	assert_select "title", "About | Ruby on rails Tutorial Sample App"
   end
   	test "should get contact" do
   	get :contact
   	assert_response :success
-  	assert_select "title", "Contact | #{@base_title}"
-  	assert_select "h1, "Contact Mee"
+  	assert_select "title", "Contact | Ruby on rails Tutorial Sample App"
+  	#assert_select "h1, "Contact Me"
+
   end
 
 end
