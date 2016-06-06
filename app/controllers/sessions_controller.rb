7c36e68933a2abf9af73fefaceedf9372d5b2850
cesswairimu <cessmbuguar@gmail.com>
2016-06-06 17:23:26 +0300

    create remember digest and login a user with remembering

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   	user = User.find_by(email: params[:session][:email].downcase)
   	if user && user.authenticate(params[:session][:password])
   		log_in  user
+      remember user
   		redirect_to user
   	else
   		flash.now[:danger] = 'Hey Bunch weka  vitu valid'
@@ -12,5 +13,7 @@ class SessionsController < ApplicationController
   end
 end
   def destroy
+    log_out
+    redirect_to root_url
   end
 end
