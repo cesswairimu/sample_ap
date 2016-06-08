993691316374dec1ce29f23585e2dd12a72dfe86
cesswairimu <cessmbuguar@gmail.com>
2016-06-08 13:31:52 +0300

    finish user edit,update,index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 def show
   		@user = User.find(params[:id])
   		
@@ -32,6 +36,11 @@ end
   render 'edit' 
 end
 end
+def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted, You happy now?"
+    redirect_to users_url
+end
   	private 
   	def user_params
   		params.require(:user).permit(:name, :email, :password,
@@ -40,6 +49,7 @@ end
 end
 def logged_in_user
   unless logged_in?
+    store_location
     flash[:danger] = "Log in acha utiaji"
     redirect_to login_url
   end
@@ -47,6 +57,10 @@ end
 def correct_user
   @user = User.find(params[:id])
   redirect_to(root_url) unless current_user?(@user)
+end
+
+def admin_user
+  redirect_to(root_url) unless current_user.admin?
 end
 
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
 	def setup
-		@user = users(:Lawrence)
+		@user = users(:Example)
 		@other_user = users(:cess)
 	end
   test "should get new" do
@@ -32,4 +32,22 @@ class UsersControllerTest < ActionController::TestCase
  	assert flash.empty?
  	assert_redirected_to root_url
  end
+ test "should redirect index when not logged in" do
+ 	get :index
+ 	assert_redirected_to login_url
+ end
+ test "should redirect destroy when not logged in" do
+ 	assert_no_difference 'User.count' do
+ 		delete :destroy, id: @user
+ 	end
+ 	assert_redirected_to login_url
+ end
+ test "should redirect destroy when logged in as a non-admin" do
+ 	log_in_as(@other_user)
+ 	assert_no_difference 'User.count' do
+ 		delete :destroy, id: @user
+ 	end
+ 	assert_redirected_to root_url
+ end
+
 end
