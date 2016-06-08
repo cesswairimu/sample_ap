2a3092ef638862e0f90dd30702b237cd85cf72f4
cesswairimu <cessmbuguar@gmail.com>
2016-06-08 14:04:46 +0300

    created a partial for edit and new file

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -49,5 +49,13 @@ class UsersControllerTest < ActionController::TestCase
  	end
  	assert_redirected_to root_url
  end
+ test "should not allow the admin attribute to be deleted" do
+ 	log_in_as(@other_user)
+ 	assert_not @other_user.admin?
+ 	patch :update, id: @other_user, user: { password:  "foobar",
+ 											password_confirmation: "foobar",
+ 											admin: "Example" }
+ 	assert_not @other_user.admin?
+ end
 
 end
