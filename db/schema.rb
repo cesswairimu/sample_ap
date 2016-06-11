9a629440a5fddf19a9d8e9e228babe877dc6b678
cesswairimu <cessmbuguar@gmail.com>
2016-06-11 11:00:26 +0300

    add micropost uploads tos to include images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609092512) do
+ActiveRecord::Schema.define(version: 20160611063817) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
