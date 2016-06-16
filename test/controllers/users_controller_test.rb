b33130a0dcd0582df69e264a2d7c36885ed99409
cesswairimu <cessmbuguar@gmail.com>
2016-06-16 09:58:45 +0300

    web interface for following users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -57,5 +57,12 @@ class UsersControllerTest < ActionController::TestCase
  											admin: "Example" }
  	assert_not @other_user.admin?
  end
+ test "should redirect following when not logged in" do 
+ 	get :following, id: @user
+ 	assert_redirected_to login_url
+ end
+ test "should redirect followers when not logged in" do 
+ 	get :followers, id: @user
+ end
 
 end
