af69905f32ce10ca265e1918964dd52f8712877b
Cess <cessmbuguar@gmail.com>
2016-06-24 10:15:21 +0300

    solving problems

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -3,43 +3,53 @@ require 'test_helper'
 class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:example)
+    @other_user = users(:lawrence)
     
   end
 
-#   test "micropost interface" do
-#     log_in_as(@user)
-#     get root_path
-#     assert_select 'div.pagination'
-#       assert_select 'input[type=text]'
-#       post microposts_path, micropost: { content: "" }
-#       assert_select 'div#error_explanation'
-#       content = "This micropost really ties the room together"
-#       picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
-#       assert_difference 'Micropost.count', 1 do
-#       post microposts_path, micropost: { content: content, picture: picture }
-#     end
-#     end
-#      assert picture.picture?
-#     follow_redirect!
-#     assert_match content, response.body
-#     assert_select 'a',  'delete'
-#     first_micropost = @user.microposts.paginate(page: 1).first
-#     assert_difference 'Micropost.count', -1 do
-#       delete micropost_path(first_micropost)
-#     end
-#     get user_path(users(:Lawrence))
-#     assert_select 'a', text: 'delete', count: 0
-#   end
-#   test "micropost sidebar count" do
-#     log_in_as(@user)
-#     get root_path
-#     assert_match "#{Example} microposts", response.body
-#     other_user = users(:Lawrence)
-#     log_in_as(other_user)
-#     get root_path
-#     assert_match "0 microposts", response.body
-#     other_user.microposts.create!(content: "A micropost")
-#     get root_path
-#     assert_match Lawrence, response.body
-# end
+  # test "micropost interface" do
+  #   log_in_as(@user)
+  #   get root_path
+  #   assert_select 'div.pagination'
+  #   assert_select 'input[type="file"]'
+  #   assert_no_difference
+  #   'Micropost.count' 
+  #       post microposts_path,
+  #       params: { micropost: { content: "" }}
+  #   end
+  #     assert_select 'div#error_explanation'
+  #     content = "This micropost really ties the room together"
+  #     picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
+  #     assert_difference 'Micropost.count', 1 do
+  #     post microposts_path, 
+  #     params: {micropost: { content: content , picture: picture}}
+  #   end
+  #   micropost = assigns(:micropost)
+  #   assert micropost.picture?
+  #   assert_redirected_to root_url
+  #       follow_redirect!
+  #   assert_match content, response.body
+
+  #   assert_select 'a', text: 'delete'
+  #   first_micropost = @user.microposts.paginate(page: 1).first
+  #   assert_difference 'Micropost.count', -1 do
+  #     delete micropost_path(first_micropost)
+  #   end
+  #   get user_path(users(:lawrence))
+  #   assert_select 'a', text: 'delete', count: 0
+  # end
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+    log_in_as(@other_user)
+    get root_path
+    assert_match "0 microposts", response.body
+    @other_user.microposts.create!(content: "A micropost")
+    get root_path
+    assert_match "#{@other_user.microposts.count} micropost", response.body 
+      end
+
 end
+
+
